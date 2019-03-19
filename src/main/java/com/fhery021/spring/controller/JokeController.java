89fc8e656dc69dcde834b233050b211aa86d5a63
Ferenc Bondar <fhery021@gmail.com>
2019-03-19 11:19:59 +0200

    modif in controller

--- src/main/java/com/fhery021/spring/controller/JokeController.java
+++ src/main/java/com/fhery021/spring/controller/JokeController.java
@@ -3,9 +3,11 @@ package com.fhery021.spring.controller;
 import com.fhery021.spring.csv.JokeReader;
 import com.fhery021.spring.model.Joke;
 import com.fhery021.spring.service.JokeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,7 @@ public class JokeController {
 
     private JokeReader jokeReader;
 
-    private JokeService jokeService;
+    private final JokeService jokeService;
 
     public JokeController(JokeReader jokeReader, JokeService service) {
         this.jokeReader = jokeReader;
